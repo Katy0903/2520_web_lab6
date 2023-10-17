6c2fadb261fd464ee74f9d0fc7ab9b965815076a
Katy Chan <arrru0903@gmail.com>
2023-10-16 22:44:15 -0700

    modified parsed function

--- IOhandler.js
+++ IOhandler.js
@@ -79,18 +79,13 @@ const grayScale = (pathIn, pathOut) => {
   
     input
       .pipe(transformStream)
-      .on("parsed", function () {
+      .on("parsed", function() {
+        // Loop through the pixel array and apply the grayscale filter
         for (let y = 0; y < this.height; y++) {
           for (let x = 0; x < this.width; x++) {
             const idx = (this.width * y + x) << 2;
-    
-            // invert color
-            this.data[idx] = 255 - this.data[idx];
-            this.data[idx + 1] = 255 - this.data[idx + 1];
-            this.data[idx + 2] = 255 - this.data[idx + 2];
-    
-            // and reduce opacity
-            this.data[idx + 3] = this.data[idx + 3] >> 1;
+            const gray = (this.data[idx] + this.data[idx + 1] + this.data[idx + 2]) / 3;
+            this.data[idx] = this.data[idx + 1] = this.data[idx + 2] = gray;
           }
         }
     
